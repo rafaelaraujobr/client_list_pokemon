fa3672d2ee55b72ec5c7da8f49b3263354d8b457
Rafael Araujo <rflaraujodev@gmail.com>
2023-01-02 23:51:00 -0300

    feat: init ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
-     
+      <Button type="primary">Button</Button>
     </div>
   );
 }
